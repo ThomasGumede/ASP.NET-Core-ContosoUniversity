28879be1d51a126da5d7b9c245d1d0674f195b63
ThomasGumede <gumedethomas@outlook.com>
2022-01-26 14:10:18 +0200

    Added pagination to index page

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -2,16 +2,19 @@ using ContosoUniversity.Models;
 using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(SchoolContext context)
+        public IndexModel(SchoolContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public string NameSort { get; set; }
@@ -19,12 +22,21 @@ namespace ContosoUniversity.Pages.Students
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             CurrentFilter = searchString;
 
@@ -32,8 +44,7 @@ namespace ContosoUniversity.Pages.Students
                                              select s;
             if (!String.IsNullOrEmpty(searchString))
             {
-                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
-                                       || s.FirstMidName.Contains(searchString));
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString) || s.FirstMidName.Contains(searchString));
             }
 
             switch (sortOrder)
@@ -52,7 +63,8 @@ namespace ContosoUniversity.Pages.Students
                     break;
             }
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Students = await PaginatedList<Student>.CreateAsync(studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
